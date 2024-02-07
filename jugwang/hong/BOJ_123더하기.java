489d18a044c32ac1fd6865c0546eb70a7f0521c6
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-02-08 00:55:57 +0900

    2문제 풀이

--- jugwang/hong/BOJ_123더하기.java
+++ jugwang/hong/BOJ_123더하기.java
@@ -1,10 +1,11 @@
 package jugwang.hong;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_123더하기 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
