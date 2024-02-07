99b61528bd60e3677e3f9f4218f1cde4873fb85e
wise_JG <59231743+Hju95@users.noreply.github.com>
2024-02-07 23:53:51 +0900

    1,2,3 더하기 문제 풀이

---
+++ jugwang/hong/BOJ_123더하기.java
@@ -0,0 +1,27 @@
+package jugwang.hong;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_123더하기 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < N; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] dp = new int[11];
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int i = 4; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+            System.out.println(dp[n]);
+        }
+    }
+}
