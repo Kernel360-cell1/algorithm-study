d81f9dd641face08b6c20ba92550b5783d8373ea
GBGreenBravo <gbgreenbravo@gmail.com>
2024-03-09 21:55:32 +0900

    feat : PGS_섬연결하기

--- minhyeop/kim/PGS_섬연결하기.java
+++ minhyeop/kim/PGS_섬연결하기.java
@@ -1,30 +1,17 @@
 package minhyeop.kim;
 
-/*
-- 49:10
-
-- 그리디
-
-- cost에 다리 정보를 모두 저장합니다.
-- 그러고 난 뒤, 다리 건설비용이 가장 높은 것부터 하나씩 빼보며
-  - check()를 통해, 해당 다리 없이도 다 연결되면 빼고, 다 연결되지 않으면 빼지 않습니다.
-- 그런 다음, cost에 남아있는 다리 비용만 계산하여 return 합니다.
-
-- 이 풀이와는 다른 방식인,
-  - 다리 건설비용 낮은 것부터 추가하며, 이미 연결돼 있다면 pass 하는 방식이 더 간결하고 합리적으로 보입니다.
- */
-
 import java.util.*;
 
 class PGS_섬연결하기 {
     static ArrayList<int[]> cost;
     public int solution(int n, int[][] costs) {
-        // 다리 정보 모두 저장
-        cost = new ArrayList<int[]>();
+        // 다리 정보 저장할 cost.
+        cost = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             cost.add(new int[n]);
         }
 
+        // 건설비용이 내림차순으로 정렬된 다리 정보 queue.
         PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> -o[2]));
 
         for (int[] c : costs) {
@@ -33,10 +20,12 @@ class PGS_섬연결하기 {
             queue.add(c);
         }
 
+        // 다리 건설비용 높은 것부터, 빼도 되는지 수행.
         while (!queue.isEmpty()) {
             check(queue.poll());
         }
 
+        // 현재 도로 정보에 대해 중복 없이 계산.
         int answer = 0;
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -54,15 +43,16 @@ class PGS_섬연결하기 {
 
         boolean[] gone = new boolean[cost.size()];
         Queue<Integer> now = new LinkedList<>();
+
+        // 0부터 시작.
         gone[0] = true;
         now.add(0);
 
         while (!now.isEmpty()) {
             int n = now.poll();
             for (int i = 0; i < cost.size(); i++) {
-                if (gone[i]) continue;
-                else {
-                    if (cost.get(n)[i] == 0) continue;
+                if (!gone[i] && (cost.get(n)[i] != 0)) {
+                    // 가본적 없고, 유효한 도로 있다면 => now Queue에 추가 & gone 처리.
                     gone[i] = true;
                     now.add(i);
                 }
@@ -77,6 +67,7 @@ class PGS_섬연결하기 {
             }
         }
 
+        // 0에서 모든 섬으로 이어지지 않으면 롤백.
         if (!allGone) {
             cost.get(road[0])[road[1]] = road[2];
             cost.get(road[1])[road[0]] = road[2];
