216db139b7424be40149959bf15a0e0d587872e5
이윤선 <yoonseon3@gmail.com>
2024-03-12 14:31:26 +0900

    Chore: BOJ_파일정리

--- yoonseon/lee/BOJ_파일정리.java
+++ yoonseon/lee/BOJ_파일정리.java
@@ -23,25 +23,4 @@ public class BOJ_파일정리 {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
-
-    /*
-    // 아래 처럼 풀면 왜 답이 틀린지 모르겠습니다ㅜㅜ..
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new HashMap();
-        list = new ArrayList<>();
-        for(int i=0; i<N; i++) {
-            String extension = br.readLine().split("\\.")[1];
-            map.put(extension, map.getOrDefault(extension, 0) + 1);
-            list.add(extension);
-        }
-
-        Collections.sort(list);
-
-        for (String element : list) {
-            System.out.println(element + " " + map.get(element));
-        }
-    }
-    */
 }
